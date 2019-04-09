ab8af6d523a8eae8dfbd06f59f5b46fbb142542d
hjbbjh <hjbbjh0521@126.com>
2019-04-09 08:33:22 +0800

    删除无用代码

--- server/server/gg-user/src/main/java/com/hjb/common/Test.java
+++
@@ -1,9 +0,0 @@
-package com.hjb.common;
-
-/**
- * ClassName: Test
- * Description:
- * Created by hjb on 2019/3/29 18:00
- */
-public class Test {
-}
--- server/server/gg-user/src/main/java/com/hjb/dao/Test.java
+++
@@ -1,9 +0,0 @@
-package com.hjb.dao;
-
-/**
- * ClassName: Test
- * Description:
- * Created by hjb on 2019/3/29 17:43
- */
-public class Test {
-}
